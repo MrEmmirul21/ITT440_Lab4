09fec9ef054effc900d41a1b1a8bbf8ce6f53580
MrEmmirul21 <emmirulmirul@gmail.com>
2021-11-26 01:16:14 +0800

    lab4

--- client.c
+++ client.c
@@ -1,55 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <string.h>      //strlen
-#include <sys/socket.h>  //socket()
-#include <arpa/inet.h>   // inet_addr()
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
-  int socket_desc;
-  struct sockaddr_in server;
-  char *message , server_reply[2000];
+   int socket_desc;
+   struct sockaddr_in server;
+   char *message , server_reply[2000];
 
-  // create socket
-  socket_desc = socket(AF_INET,SOCK_STREAM,0);
+   // create socket
+   socket_desc = socket(AF_INET,SOCK_STREAM,0);
 
-  if (socket_desc == -1)
-  {
-    printf("Could not create socket");
-  }
+   if(socket_desc == -1)
+   {
+      printf("Could not create socket");
+   }
 
-  server.sin_addr.s_addr = inet_addr("192.168.170.13");  // ip address of ubuntu server
-  server.sin_family = AF_INET;
-  server.sin_port = htons( 8888 );
+   // prepare the sockaddr_in structure
+   server.sin_addr.s_addr = inet_addr("192.168.170.13");
+   server.sin_family = AF_INET;
+   server.sin_port = htons(8888);
 
-  // connect to remote server(ubuntu server)
-  if (connect(socket_desc,(struct sockaddr *)&server,sizeof(server)) < 0)
-  {
-    puts("Error connection");
-    return 1;
-  }
+   // connect to remote server
+   if(connect(socket_desc,(struct sockaddr*)&server,sizeof(server)) < 0)
+   {
+      puts("Error connestion");
+      return 1;
+   }
 
-  puts("Connected\n");
+   puts("Connected :)\n");
 
-  // send data to remote server
-  message = "connect";
-  if(send(socket_desc,message,strlen(message),0) < 0)
-  {
-    puts("Sending failed\n");
-    return 1;
-  }
-  puts("Sending successfull\n");
+   // send data to remote server
+   message = "connect";
+   if(send(socket_desc,message,strlen(message),0) < 0)
+   {
+      puts("Sending failed");
+      return 1;
+   }
 
-  // receive data from remote server
-  if(recv(socket_desc,server_reply,2000,0) < 0)
-  {
-    puts("Receiving failed\n");
-  }
-  puts("Receiving successfull\n");
-  puts(server_reply);
+   puts("Sending successfull\n");
 
-  close(socket_desc);
+   // receive a reply from server
+   if(recv(socket_desc,server_reply,2000,0) < 0)
+   {
+      puts("Receiving failed");
+   }
 
-  return 0;
+   puts("Receive successfull");
+   puts(server_reply);
+
+   // close socket
+   close(socket_desc);
+
+   return 0;
 }
+
